426ec48277f0530a0f1508c87ece6c8504bba00b
yennhi <yennhii.dyn@gmail.com>
2025-11-10 13:14:02 +0700

    delete garbage

--- iframeVideo/player.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("message", (e) => {
-    if (e.data?.type === "LOAD_VIDEO") {
-        const video = document.querySelector("video");
-        const container = document.querySelector(".container");
-        container.classList.remove("hidden");
-        video.src = e.data.url;
-        video.play();
-    }
-});
